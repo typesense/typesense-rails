4b5e678986065c2493c4a2a44709edec31e99fb0
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-01-28 11:30:10 +0200

    refactor(pagination): migrate pagination module from algolia to typesense
    - Update module name from `AlgoliaSearch` to `Typesense`
    - Change autoload paths to use `typesense` namespace
    - Update const_get references to use `Typesense` constant
    - Update configuration reference to use typesense configuration

--- lib/algoliasearch/pagination.rb
+++ lib/algoliasearch/pagination.rb
@@ -1,15 +1,15 @@
-module AlgoliaSearch
+module Typesense
   module Pagination
 
-    autoload :WillPaginate, 'algoliasearch/pagination/will_paginate'
-    autoload :Kaminari, 'algoliasearch/pagination/kaminari'
-    autoload :Pagy, 'algoliasearch/pagination/pagy'
+    autoload :WillPaginate, 'typesense/pagination/will_paginate'
+    autoload :Kaminari, 'typesense/pagination/kaminari'
+    autoload :Pagy, 'typesense/pagination/pagy'
 
     def self.create(results, total_hits, options = {})
-      return results if AlgoliaSearch.configuration[:pagination_backend].nil?
+      return results if Typesense.configuration[:pagination_backend].nil?
       begin
-        backend = AlgoliaSearch.configuration[:pagination_backend].to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase } # classify pagination backend name
-        page = Object.const_get(:AlgoliaSearch).const_get(:Pagination).const_get(backend).create(results, total_hits, options)
+        backend = Typesense.configuration[:pagination_backend].to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase } # classify pagination backend name
+        page = Object.const_get(:Typesense).const_get(:Pagination).const_get(backend).create(results, total_hits, options)
         page
       rescue NameError
         raise(BadConfiguration, "Unknown pagination backend")
