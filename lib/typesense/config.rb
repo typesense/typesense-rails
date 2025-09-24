9db3c0afb201e00b0dfebe65a1fe0d832dc8b6c0
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-09-24 10:27:17 +0300

    feat(logger): add configurable log levels and default to warn

--- lib/typesense/config.rb
+++ lib/typesense/config.rb
@@ -11,11 +11,37 @@ module Typesense
 
     def configuration=(configuration)
       @@pagination_backend = configuration[:pagination_backend] if configuration.key?(:pagination_backend)
+      @@log_level = configuration[:log_level] if configuration.key?(:log_level)
       @@configuration = configuration
+      
+      Rails.logger.level = log_level_to_const(configuration[:log_level])
     end
 
     def pagination_backend
       @@pagination_backend
+    end
+
+    def log_level
+      @@log_level
+    end
+
+    def log_level_to_const(level)
+      case level
+      when :debug
+        Logger::DEBUG
+      when :info
+        Logger::INFO
+      when :warn
+        Logger::WARN
+      when :error
+        Logger::ERROR
+      when :fatal
+        Logger::FATAL
+      when :unknown
+        Logger::UNKNOWN
+      else
+        Logger::WARN # default fallback
+      end
     end
 
     def client
